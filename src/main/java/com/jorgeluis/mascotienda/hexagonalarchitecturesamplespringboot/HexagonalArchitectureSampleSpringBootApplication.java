4d0b576fdeb9659a228979b92adc9c28a888c86f
cjorgeluis122333 <cjorgeluis122333@gmail.com>
2026-01-19 12:22:34 -0500

    Init commit

---
+++ src/main/java/com/jorgeluis/mascotienda/hexagonalarchitecturesamplespringboot/HexagonalArchitectureSampleSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.jorgeluis.mascotienda.hexagonalarchitecturesamplespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalArchitectureSampleSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexagonalArchitectureSampleSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jorgeluis/mascotienda/hexagonalarchitecturesamplespringboot/HexagonalArchitectureSampleSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorgeluis.mascotienda.hexagonalarchitecturesamplespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalArchitectureSampleSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
