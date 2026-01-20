69d2303067de9e1a48cb864e5f09e8ba6db5f7cb
cjorgeluis122333 <cjorgeluis122333@gmail.com>
2026-01-20 17:38:41 -0500

    Hill use case, service, and adepter

--- src/main/java/com/jorgeluis/mascotienda/hexagonalarchitecturesamplespringboot/infrastructure/config/LoanConfig.java
+++ src/main/java/com/jorgeluis/mascotienda/hexagonalarchitecturesamplespringboot/infrastructure/config/LoanConfig.java
@@ -1,15 +1,53 @@
 package com.jorgeluis.mascotienda.hexagonalarchitecturesamplespringboot.infrastructure.config;
 
+import com.jorgeluis.mascotienda.hexagonalarchitecturesamplespringboot.application.usecase.CreateLoanUseCaseImplement;
+import com.jorgeluis.mascotienda.hexagonalarchitecturesamplespringboot.application.usecase.DeleteLoanUseCaseImplement;
+import com.jorgeluis.mascotienda.hexagonalarchitecturesamplespringboot.application.usecase.FindLoanUseCaseImplement;
+import com.jorgeluis.mascotienda.hexagonalarchitecturesamplespringboot.application.usecase.UpdateLoanUseCaseImplement;
+import com.jorgeluis.mascotienda.hexagonalarchitecturesamplespringboot.domain.ports.in.usecase.CreateLoanUseCase;
+import com.jorgeluis.mascotienda.hexagonalarchitecturesamplespringboot.domain.ports.in.usecase.DeleteLoanUseCase;
+import com.jorgeluis.mascotienda.hexagonalarchitecturesamplespringboot.domain.ports.in.usecase.FindLoanUseCase;
+import com.jorgeluis.mascotienda.hexagonalarchitecturesamplespringboot.domain.ports.in.usecase.UpdateLoanUseCase;
 import com.jorgeluis.mascotienda.hexagonalarchitecturesamplespringboot.domain.ports.out.LoanRepositoryPort;
-import com.jorgeluis.mascotienda.hexagonalarchitecturesamplespringboot.domain.service.CreateLoanService;
+import com.jorgeluis.mascotienda.hexagonalarchitecturesamplespringboot.infrastructure.output.persisten.adapter.JpaLoanAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LoanConfig {
+
+    //========================================    PORT to ADAPTER
+    // (domain to infrastructure)
     @Bean
-    public CreateLoanService createLoanDomainService(LoanRepositoryPort loanRepositoryPort) {
-        // Ahora Java sí encuentra un constructor que acepte LoanRepositoryPort
-        return new CreateLoanService(loanRepositoryPort);
+    LoanRepositoryPort loanRepositoryPort(JpaLoanAdapter jpaLoanAdapter) {
+        return jpaLoanAdapter;
     }
+
+
+    //=======================================    USE CASES
+    //Bind (Domain to Infrastructure)
+    //UseCase  If received a param you have to  pass the param of the attribute in the param of the methode
+    @Bean
+    CreateLoanUseCase createLoanUseCase(LoanRepositoryPort loanRepositoryPort) {
+        return new CreateLoanUseCaseImplement(loanRepositoryPort);
+    }
+
+    @Bean
+    DeleteLoanUseCase deleteLoanUseCase(LoanRepositoryPort loanRepositoryPort) {
+        return new DeleteLoanUseCaseImplement(loanRepositoryPort);
+    }
+
+    @Bean
+    FindLoanUseCase findLoanUseCase(LoanRepositoryPort loanRepositoryPort) {
+        return new FindLoanUseCaseImplement(loanRepositoryPort);
+    }
+
+    @Bean
+    UpdateLoanUseCase updateLoanUseCase(LoanRepositoryPort loanRepositoryPort) {
+        return new UpdateLoanUseCaseImplement(loanRepositoryPort);
+    }
+    //=======================================    Service
+    //Application to Application  <-- Domain  (Require domain interface)
+
+
 }
